aa707ef426b6e2b71ff41d8785499a52781a3530
Janey Dirk <ivakolganov@gmail.com>
2026-02-19 21:02:14 +0300

    added a few changes to sum and diff functions

--- src/str_math/str_sum_and_diff.c
+++ src/str_math/str_sum_and_diff.c
@@ -5,7 +5,59 @@
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
-static int str_remove_trailing_zeros(char *s) {
+static int str_add_to_result(char *s, int k, char *str1, int i, char *str2, int j, bool is_sum, int back_to_digits, int adjust_next_digit) {
+    int shift = 0;
+    if (str2 == NULL)
+        j = i + 1;
+    for (; i >= 0 && j >= 0; i--, j--) {
+        s[k] += str1[i];
+        if (str2 != NULL && is_sum) {
+            s[k] += str2[j] - '0';
+        } else if (str2 != NULL && !is_sum) {
+            s[k] += '0' - str2[j];
+        }
+        if ((is_sum && s[k] >= 10 + '0') || (!is_sum && s[k] < '0')) {
+            s[k] += back_to_digits;
+            s[k - 1] += adjust_next_digit;
+        }
+        k--;
+        shift++;
+    }
+    return shift;
+}
+
+static int str_process_fraction(char *s, char *str1, char *str2, int *ii, int *jj, int k) {
+    int i = *ii;
+    int j = *jj;
+    char *point1 = strchr(str1, '.');
+    char *point2 = strchr(str2, '.');
+    int frac_digits1 = i - (int)((point1 != NULL ? point1 : str1 + i) - str1);
+    int frac_digits2 = j - (int)((point2 != NULL ? point2 : str2 + j) - str2);
+    char *str;
+    int iter_amount;
+
+    if (frac_digits1 > frac_digits2) {
+        str = str1;
+        iter_amount = frac_digits1 - frac_digits2;
+    } else {
+        str = str2;
+        iter_amount = frac_digits2 - frac_digits1;
+        i = j;
+    }
+    for (int iter = 0; iter < iter_amount; iter++) {
+        s[k] = str[i];
+        i--;
+        k--;
+    }
+    // int shift = str_add_to_result(s, k, str1, i, str2, j, );
+    if (frac_digits1 > frac_digits2)
+        *ii = i;
+    else
+        *jj = i;
+    return k;
+}
+
+static int str_remove_zeros_at_beginning(char *s) {
     int k = 0;
 
     while (s[k] == '0')
@@ -17,42 +69,36 @@ static int str_remove_trailing_zeros(char *s) {
     return k;
 }
 
+// static void str_remove_trailing_zeros(char *s) {
+//     for (int i = 0; s[i] == '0' || s[i] == '.'; i--)
+//         s[i] = '\0';
+// }
+
 static int str_positive_sum(char *dst, char *str1, char *str2) {
     int i = strlen(str1) - 1, j = strlen(str2) - 1;
     int s_size = max(i, j) + 3;
     char *s = (char *) calloc(s_size, sizeof(char));
-    int k = s_size - 2;
-    for (; i >= 0 && j >= 0; i--, j--) {
-        s[k] += str1[i] + str2[j] - '0';
-        if (s[k] >= 10 + '0') {
-            s[k] -= 10 ;
-            s[k - 1] += 1;
-        }
-        k--;
-    }
-    char *ostr;
-    int ostr_len;
+    int k = str_process_fraction(s, str1, str2, &i, &j, s_size - 2);
+    int shift = str_add_to_result(s, k, str1, i, str2, j, true, -10, 1);
+    k -= shift;
+    i -= shift;
+    j -= shift;
+
+    char *ostr = str2;
+    int ostr_len = j;
     if (i > j) {
         ostr = str1;
         ostr_len = i;
-    } else {
-        ostr = str2;
-        ostr_len = j;
     }
-    for (i = ostr_len; i >= 0; i--) {
-        s[k] += ostr[i];
-        if (s[k] >= 10 + '0') {
-            s[k] -= 10;
-            s[k - 1] += 1;
-        }
-        k--;
-    }
+    
+    shift = str_add_to_result(s, k, ostr, ostr_len, NULL, 0, true, -10, 1);
+    k -= shift;
     if (s[k] != 0) {
         s[k] += '0';
         k--;
     }
     k++;
-    k += str_remove_trailing_zeros(s + k);
+    k += str_remove_zeros_at_beginning(s + k);
     strcpy(dst, s + k);
     free(s);
     return s_size - k - 1;
@@ -69,25 +115,16 @@ static int str_positive_difference(char *dst, char *str1, char *str2) {
     int i = strlen(str1) - 1, j = strlen(str2) - 1;
     int s_size = max(i, j) + 3;
     char *s = (char *) calloc(s_size, sizeof(char));
-    int k = s_size - 2;
-    for (; i >= 0 && j >= 0; i--, j--) {
-        s[k] += str1[i] - str2[j] + '0';
-        if (s[k] < '0') {
-            s[k] += 10 ;
-            s[k - 1] -= 1;
-        }
-        k--;
-    }
-    for (; i >= 0; i--) {
-        s[k] += str1[i];
-        if (s[k] < '0') {
-            s[k] += 10;
-            s[k - 1] -= 1;
-        }
-        k--;
-    }
+    int k = str_process_fraction(s, str1, str2, &i, &j, s_size - 2);
+    int shift = str_add_to_result(s, k, str1, i, str2, j, false, 10, -1);
+    k -= shift;
+    i -= shift;
+    j -= shift;
+
+    shift = str_add_to_result(s, k, str1, i, NULL, 0, false, 10, -1);
+    k -= shift;
     k++;
-    k += str_remove_trailing_zeros(s + k);
+    k += str_remove_zeros_at_beginning(s + k);
     strcpy(dst + negative, s + k);
     if (negative)
         dst[0] = '-';
@@ -114,6 +151,8 @@ static int str_handle_sum_or_difference(char *dst, char *str1, char *str2, bool 
         }
         res = str_positive_difference(dst, str1 + sign1, str2 + sign2);
     }
+    // if (strchr(dst, '.') != NULL)
+    //     str_remove_trailing_zeros(dst + res - 1);
     return res;
 }
 
